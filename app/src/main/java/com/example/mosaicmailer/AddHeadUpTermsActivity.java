d4067fc4615a02a952dc2290b4a120bc15a6d777
kyprog <s18t312@kagawa-u.ac.jp>
2022-10-04 03:16:56 +0900

    注意喚起メールの条件となるメールアドレスとキーワードを入れる機能を実装した

--- app/src/main/java/com/example/mosaicmailer/AddHeadUpTermsActivity.java
+++ app/src/main/java/com/example/mosaicmailer/AddHeadUpTermsActivity.java
@@ -1,11 +1,16 @@
 package com.example.mosaicmailer;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class AddHeadUpTermsActivity extends AppCompatActivity {
+    MosaicMailerDatabaseHelper helper = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,24 @@ public class AddHeadUpTermsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        helper = new MosaicMailerDatabaseHelper(this);
+
+    }
+
+    public void addTermsOnClick(View view){
+        String headsUpMailaddress = ((EditText) findViewById(R.id.headsUpMailAddress)).getText().toString();
+        String headsUpKeyword = ((EditText) findViewById(R.id.headsUpKeyword)).getText().toString();
+
+        try {
+            SQLiteDatabase db = helper.getWritableDatabase();
+            ContentValues cv = new ContentValues();
+            cv.put("mailaddress", headsUpMailaddress);
+            cv.put("keyword", headsUpKeyword);
+            db.insert("HeadsUpInfo", null, cv);
+            //db.insertWithOnConflict("books", null, cv, SQLiteDatabase.CONFLICT_REPLACE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     //戻るボタンで戻る
